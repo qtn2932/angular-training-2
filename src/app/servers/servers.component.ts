596a917fc7d278a793ea9463632773b02ff0d5e2
Quan Nguyen <quan.nguyen199712@gmail.com>
2020-08-23 22:23:30 -0400

    added click event listerner

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer:boolean = false
+  serverCreationStatus ="No server was created"
   constructor() { 
     setTimeout(()=>{this.allowNewServer=true},2000)
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created';
   }
 
 }
